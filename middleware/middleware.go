b7c24d190f5947e49051e46b4668bb524dad5e28
zhitongLIU <zhitonggm.liu@gmail.com>
2018-11-04 17:59:27 +0100

    Add middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/zhitongLIU/lentslog/models"
+	"net/http"
+	"os"
+)
+
+func CertifyApp(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slackKey := os.Getenv("SLACK_APP_TOKEN")
+
+		request, err := models.NewSlackRequest(r)
+
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+
+		if slackKey != request.Token {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - Not right key"))
+			return
+		}
+		handler.ServeHTTP(w, r)
+	}
+}
