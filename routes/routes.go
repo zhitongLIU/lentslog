f5db8eca97d1c12288b52ab4b066f17c2b424896
zhitongLIU <zhitonggm.liu@gmail.com>
2018-11-04 18:02:06 +0100

    Use middleware in routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/zhitongLIU/lentslog/middleware"
 	"github.com/zhitongLIU/lentslog/routes/lents"
 )
 
 func NewRouter() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/lents", lents.LentsPostHandler).Methods("POST")
+	r.HandleFunc("/lents", middleware.CertifyApp(lents.LentsPostHandler)).Methods("POST")
 
 	return r
 }
