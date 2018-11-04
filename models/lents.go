69bdfdf27641e73cddaa41e08907e3f5c879c5e6
zhitongLIU <zhitonggm.liu@gmail.com>
2018-11-04 17:58:52 +0100

    Add models lents

---
+++ models/lents.go
@@ -0,0 +1,16 @@
+// Package ge slack
+
+package models
+
+type Trancation struct {
+	Id   int64
+	Sum  float64
+	From string
+	To   string
+}
+
+// &SlackRequest{
+// 	Sum:  sum,
+// 	From: from,
+// 	To:   to,
+// }
