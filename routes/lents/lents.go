f512a1fe644a63a98ff93f8815003e02dc615bde
zhitongLIU <zhitonggm.liu@gmail.com>
2018-11-04 18:02:59 +0100

    Use request in lents route

--- routes/lents/lents.go
+++ routes/lents/lents.go
@@ -2,15 +2,27 @@ package lents
 
 import (
 	"fmt"
+	"github.com/zhitongLIU/lentslog/models"
 	"net/http"
-	"net/http/httputil"
 )
 
 func LentsPostHandler(w http.ResponseWriter, r *http.Request) {
-	request, err := httputil.DumpRequest(r, true)
+	r.ParseForm()
+	request, err := models.NewSlackRequest(r)
+
 	if err != nil {
+		fmt.Fprintf(w, err.Error())
 		return
 	}
 
-	fmt.Fprintf(w, string(request))
+	fmt.Printf("%s = %s\n", "ChannelId", request.ChannelId)
+	fmt.Printf("%s = %s\n", "Token", request.Token)
+	fmt.Printf("%s = %s\n", "TriggerId", request.TriggerId)
+	fmt.Printf("%s = %s\n", "TeamId", request.TeamId)
+	fmt.Printf("%s = %s\n", "TeamDomain", request.TeamDomain)
+	fmt.Printf("%s = %s\n", "UserId", request.UserId)
+	fmt.Printf("%s = %s\n", "Text", request.Text)
+	fmt.Printf("%s = %s\n", "ChannelName", request.ChannelName)
+	fmt.Printf("%s = %s\n", "Time", request.Time)
+	fmt.Printf("%s = %s\n", "UserName", request.UserName)
 }
